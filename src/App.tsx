18cc181f7056a85170c37b990439971e0b8db2b3
Kuznetsov Ilia <killxxxvi@gmail.com>
2023-06-25 18:06:54 +0200

    hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
 import UserResultPage from "./pages/UserResultPage";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" Component={SearchPage} />
         <Route path="/user/:username" Component={UserResultPage} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
